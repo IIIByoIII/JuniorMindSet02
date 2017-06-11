a39f4254bc57bfbb63052199033379b9d5a49e05
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 12:04:33 +0300

    Created test for over 3 weight
    Also adjusted indentations everywhere (working with 2 spaces, I like it more)

--- Watermelon/WatermelonTests.cs
+++ Watermelon/WatermelonTests.cs
@@ -15,22 +15,35 @@ namespace Watermelon
     [TestMethod]
     public void WeightIsOdd()
     {
-        Assert.AreEqual("NU", WeightDivision(5));
+      Assert.AreEqual("NU", WeightDivision(5));
+    }
+
+    [TestMethod]
+    public void WeightIsOverThree()
+    {
+      Assert.AreEqual("NU", WeightDivision(2));
     }
 
     string WeightDivision (int watermelonMass)
     {
-      string divideEqually;
-      int massIsOdd = watermelonMass % 2;
-      if ( massIsOdd == 1 )
+      if ( watermelonMass > 3 )
       {
+        string divideEqually;
+        int massIsOdd = watermelonMass % 2;
+        if ( massIsOdd == 1 )
+        {
           divideEqually = "NU";
+        }
+        else
+        {
+          divideEqually = "DA";
+        }
+        return divideEqually;
       }
       else
       {
-          divideEqually = "DA";
+        return "NU";
       }
-      return divideEqually;
     }
   }
 }
