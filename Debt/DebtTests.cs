3fc43e27324cf8c92214700b865e0125b6cf3b8b
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 13:44:51 +0300

    Added PaidUpToTenDays test

--- Debt/DebtTests.cs
+++ Debt/DebtTests.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Debt
 {
   [TestClass]
-  public class UnitTest1
+  public class DebtTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void PaidUpToTenDays()
     {
+      Assert.AreEqual(110, RentAndDebt(100, 5));
+    }
+
+    decimal RentAndDebt (decimal rentValue, uint daysLate)
+    {
+      decimal totalToPay = 0;
+      totalToPay = rentValue + daysLate * (rentValue * 0.02m);
+      return totalToPay;
     }
   }
 }
