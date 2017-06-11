2f58c4b8c475d60c58b1439273a7f8494183f292
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 13:48:18 +0300

    Added PaidUpToThirtyDays

--- Debt/DebtTests.cs
+++ Debt/DebtTests.cs
@@ -12,10 +12,23 @@ namespace Debt
       Assert.AreEqual(110, RentAndDebt(100, 5));
     }
 
+    [TestMethod]
+    public void PaidUpToThirtyDays()
+    {
+      Assert.AreEqual(175, RentAndDebt(100, 15));
+    }
+
     decimal RentAndDebt (decimal rentValue, uint daysLate)
     {
       decimal totalToPay = 0;
-      totalToPay = rentValue + daysLate * (rentValue * 0.02m);
+      if (daysLate < 11)
+      {
+        totalToPay = rentValue + daysLate * (rentValue * 0.02m);
+      }
+      else if (daysLate < 31)
+      {
+        totalToPay = rentValue + daysLate * (rentValue * 0.05m);
+      }
       return totalToPay;
     }
   }
