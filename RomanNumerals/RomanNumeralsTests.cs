d87067126fa592fe5d9782ae1d72f8aa2d813a1d
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 14:05:19 +0300

    Added GetRomanNumeralFour test

--- RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests.cs
@@ -7,8 +7,31 @@ namespace RomanNumerals
   public class RomanNumeralsTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void GetRomanNumeralFour()
     {
+        Assert.AreEqual("IV", ConvertToRomanNumerals(4));
+    }
+
+    string ConvertToRomanNumerals (int numberToConvert)
+    {
+      string[] numeralCharacters = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
+      int[] numeralValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+      Array.Reverse(numeralCharacters);
+      Array.Reverse(numeralValues);
+      int numberDivision;
+      int i = 0;
+      string romanNumeral = "";
+      foreach (int currentValue in numeralValues) 
+      {
+        numberDivision = numberToConvert / currentValue;
+        if (numberDivision == 1)
+        {
+          romanNumeral = $"{romanNumeral}{numeralCharacters[i]}";
+          numberToConvert -= currentValue;
+        }
+        i++;
+      }
+      return romanNumeral;
     }
   }
 }
