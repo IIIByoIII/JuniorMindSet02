8adfd82a9fc0337f77b5d2a47350330fad0f6c09
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 13:26:42 +0300

    Added WillItBuzz test

--- FizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzzTests.cs
@@ -12,13 +12,24 @@ namespace FizzBuzz
       Assert.AreEqual("Fizz", ShowFizzBuzz(6));
     }
 
+    [TestMethod]
+    public void WillItBuzz()
+    {
+      Assert.AreEqual("Buzz", ShowFizzBuzz(10));
+    }
+
     string ShowFizzBuzz (int inputNumber)
     {
       string variableFizzBuzz = "";
       int remainderFizz = inputNumber % 3;
+      int remainderBuzz = inputNumber % 5;
       if (remainderFizz == 0)
       {
         variableFizzBuzz = "Fizz";
+      }
+      if (remainderBuzz == 0)
+      {
+        variableFizzBuzz = "Buzz";
       }
       return variableFizzBuzz;
     }
