1d6b7494364cbb9a991ff798a50f32f5758ac168
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 11:49:57 +0300

    Create new WatermelonTests project

---
+++ Watermelon/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Watermelon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Watermelon")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("833d5167-181f-4095-a765-2fed1a53a0ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Watermelon/WatermelonTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Watermelon
+{
+  [TestClass]
+  public class WatermelonTests
+  {
+    [TestMethod]
+    public void UnitTest1()
+    {
+    }
+  }
+}
