1e464458dfb2e1048f04a03825d0eac25dc3f5fd
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 14:16:40 +0300

    Added GetRomanNumeralFourtyFive test

--- RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests.cs
@@ -48,6 +48,12 @@ namespace RomanNumerals
         Assert.AreEqual("XLI", ConvertToRomanNumerals(41));
     }
 
+    [TestMethod]
+    public void GetRomanNumeralFourtyFive()
+    {
+        Assert.AreEqual("XLV", ConvertToRomanNumerals(45));
+    }
+
     string ConvertToRomanNumerals (int numberToConvert)
     {
       string[] numeralCharacters = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
