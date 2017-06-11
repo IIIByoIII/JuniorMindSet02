65c40012a3ec432ad007fb630892aa330599b0f4
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 14:10:55 +0300

    Added GetRomanNumeralFifteen test

--- RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests.cs
@@ -24,6 +24,12 @@ namespace RomanNumerals
         Assert.AreEqual("IX", ConvertToRomanNumerals(9));
     }
 
+    [TestMethod]
+    public void GetRomanNumeralFifteen()
+    {
+        Assert.AreEqual("XV", ConvertToRomanNumerals(15));
+    }
+
     string ConvertToRomanNumerals (int numberToConvert)
     {
       string[] numeralCharacters = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
