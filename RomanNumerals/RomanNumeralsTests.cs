7769d1f64faa337937eba975213413b5832324b0
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 14:09:38 +0300

    Added GetRomanNumeralNine test

--- RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests.cs
@@ -18,6 +18,12 @@ namespace RomanNumerals
         Assert.AreEqual("VIII", ConvertToRomanNumerals(8));
     }
 
+    [TestMethod]
+    public void GetRomanNumeralNine()
+    {
+        Assert.AreEqual("IX", ConvertToRomanNumerals(9));
+    }
+
     string ConvertToRomanNumerals (int numberToConvert)
     {
       string[] numeralCharacters = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
