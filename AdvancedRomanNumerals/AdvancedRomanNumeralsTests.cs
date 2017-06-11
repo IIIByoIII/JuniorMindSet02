28090d446ebf6a5af1a3575ee1d48eda93b7af56
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 14:19:59 +0300

    Created AdvancedRomanNumerals project

---
+++ AdvancedRomanNumerals/AdvancedRomanNumeralsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdvancedRomanNumerals
+{
+  [TestClass]
+  public class AdvancedRomanNumeralsTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
