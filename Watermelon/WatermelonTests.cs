dc70ca8f9f3dfe92ac8906ec30861ef6511f7918
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 11:55:47 +0300

    Created test for even weight

--- Watermelon/WatermelonTests.cs
+++ Watermelon/WatermelonTests.cs
@@ -7,8 +7,24 @@ namespace Watermelon
   public class WatermelonTests
   {
     [TestMethod]
-    public void UnitTest1()
+    public void WeightIsEven()
     {
+      Assert.AreEqual("DA", WeightDivision(4));
+    }
+
+    string WeightDivision (int watermelonMass)
+    {
+      string divideEqually;
+      int massIsOdd = watermelonMass % 2;
+      if ( massIsOdd == 1 )
+      {
+          divideEqually = "NU";
+      }
+      else
+      {
+          divideEqually = "DA";
+      }
+      return divideEqually;
     }
   }
 }
