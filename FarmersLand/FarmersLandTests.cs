7bdad691bb6072670284d7d843f7bfb166f5ad83
IIIByoIII <robertkiss26@gmail.com>
2017-06-11 13:08:58 +0300

    Created LandArea test

--- FarmersLand/FarmersLandTests.cs
+++ FarmersLand/FarmersLandTests.cs
@@ -7,8 +7,16 @@ namespace FarmersLand
   public class FarmersLandTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void LandArea()
     {
+      Assert.AreEqual(592900d, InitialLandArea());
+    }
+
+    double InitialLandArea()
+    {
+      double initialLength = (-230 + Math.Sqrt(230 * 230 - 4 * -770000)) / 2;
+      double initialArea = Math.Pow(initialLength, 2);
+      return initialArea;
     }
   }
 }
